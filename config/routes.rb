9d059ba607d5fb1e9c58dc840174f834d228d0f0
imbaniac <tima666tima@gmail.com>
2016-08-27 15:16:33 +0300

    App core logic added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: []
+  get 'statistics', to: 'analyze_data#statistics'
+	get 'correlations', to: 'analyze_data#correlations'
+	resource :login, only: [:create], controller: :sessions
 end
