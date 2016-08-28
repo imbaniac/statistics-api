1191debd3c66eccd4fcb1f1c53ce552410ccf111
imbaniac <tima666tima@gmail.com>
2016-08-28 16:09:59 +0300

    CORS bugfix in config

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module Statdata
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+		config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'http://localhost:3000',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
   end
 end
