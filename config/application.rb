09892c52d8d4613ebe31732675a54f80a19f0f8f
imbaniac <tima666tima@gmail.com>
2016-08-28 17:02:03 +0300

    Damn CORS

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,9 @@ module Statdata
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-		config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(","),
-		'Access-Control-Allow-Headers' => %w{Origin, Content-Type, X-Auth-Token}.join(",")
-  }
+		config.action_dispatch.default_headers.merge!({
+		  'Access-Control-Allow-Origin' => '*',
+		  'Access-Control-Request-Method' => '*'
+		})
   end
 end
